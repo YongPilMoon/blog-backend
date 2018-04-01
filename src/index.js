0ac04db400f474d105c7a0ab28916dac99bf0ebb
YongPilMoon <myp7@urbanbase.com>
2018-04-01 16:35:21 +0900

    관리자 로그인 추가

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const session = require('koa-session');
+const cors = require('@koa/cors');
+
+const {
+  PROT: port = 4000,
+  MONGO_URI: mongoURI,
+  COOKIE_SIGN_KEY: signKey,
+} = process.env;
 
 const app = new Koa();
 const router = new Router();
@@ -12,7 +20,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(mongoURI, {
 }).then((response) => {
   console.log('Successfully connected to mongodb');
 }).catch((e) => {
@@ -20,37 +28,26 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 
-app.use((ctx, next) => {
-  const allowedHosts = [
-    'localhost:3000',
-    'myp-blog.s3-website.ap-northeast-2.amazonaws.com',
-  ];
-  const origin = ctx.header.origin;
-  allowedHosts.every((el) => {
-    if(!origin) return false;
-    if(origin.indexOf(el) !== -1) {
-      ctx.response.set('Access-Control-Allow-Origin', origin);
-      return false;
-    }
-    return true;
-  });
-  ctx.set('Access-Control-Allow-Credentials', true);
-  ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-timebase, Link');
-  ctx.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, OPTIONS');
-  ctx.set('Access-Control-Expose-Headers', 'Link, Last-page');
-  return next();
-});
-
-const port = process.env.PORT || 4000;
-
-router.use('/api', api.routes());
+app.use(cors({
+  origin: '*',
+  credentials: true,
+}));
 
 app.use(bodyParser());
+
+const sessionConfig = {
+  maxAge: 86400000,
+};
+
+app.use(session(sessionConfig, app));
+app.keys = [signKey];
 
 app.use(router.routes())
   .use(router.allowedMethods());
 
-app.listen(4000, () => {
+router.use('/api', api.routes());
+
+app.listen(port, () => {
   console.log('heurm server is listening to port 4000');
 });
 
