31fbb6415a6d34fc5b2f1d5a96c43692d3c1d739
yongpilMoon <m71661@gmail.com>
2018-09-16 17:24:29 +0900

    배포파일 수정

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
+#  환경설정 파일 복사
+#  scp -i /Users/myp/aws/key/Yongpil.pem .env ubuntu@ec2-52-78-38-76.ap-northeast-2.compute.amazonaws.com:~/
 
 ssh -i /Users/myp/aws/key/Yongpil.pem ubuntu@ec2-52-78-38-76.ap-northeast-2.compute.amazonaws.com << 'ENDSSH'
 rm -rf blog-backend
 git clone https://github.com/YongPilMoon/blog-backend.git
 cd blog-backend
+yarn install
+cp ../.env .
+pm2 delete all
 npm start
 ENDSSH
+
 
